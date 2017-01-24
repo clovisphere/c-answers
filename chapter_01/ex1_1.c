088bce6daef3a6a08a92bfd1ca9763104140c0b5
Clovis Mugaruka <clovis.mugaruka@gmail.com>
2017-01-24 16:09:59 +0300

    change 'parentheses' to 'parenthese' in the comment section

--- chapter_01/ex1_1.c
+++ chapter_01/ex1_1.c
@@ -4,7 +4,7 @@
  * starts program execution. 
  */
  int main(void) {
- 	// omit the closing parentheses ')' as well as the ';'
+ 	// omit the closing parenthese ')' as well as the ';'
  	// to see what error will be displayed.
 	printf("hello, world\n" //);
 	return 0;
