c32177d928fae58516ce6aa48cc441191c3ea525
Clovis Mugaruka <clovis.mugaruka@gmail.com>
2020-08-18 12:36:13 +0300

    add: solution to exercise 1-10

---
+++ chapter_01/ex1_10.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+// starts program execution
+int main() {
+	int c;
+	while((c = getchar()) != EOF) {
+		if(c == '\t') {
+			printf("\\t");
+		} else if(c == '\b') {
+			printf("\\b");
+		} else if(c == '\\') {
+			printf("\\\\");
+		} else {
+			putchar(c);
+		}
+	}
+	return 0;
+}
