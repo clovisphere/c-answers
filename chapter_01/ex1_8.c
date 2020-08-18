5e52acbabed98ccc992435eabc1edc5369ae535d
Clovis Mugaruka <clovis.mugaruka@gmail.com>
2020-08-18 12:04:01 +0300

    add: solution to exercise 1-8

---
+++ chapter_01/ex1_8.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+// starts program execution
+int main() {
+	int c; // character
+	int nl, bl, tb; // newline, blank, tabs
+
+	nl = bl = tb = 0;
+	while((c = getchar()) != EOF) {
+		if (c == ' ') {
+			++bl;
+		} else if (c == '\t') {
+			++tb;
+		} else if (c == '\n') {
+			++nl;
+		}
+	}
+	printf("No. of blanks:   %3d\nNo. of tabs:     %3d\nNo. of newlines: %3d\n", bl, tb, nl);
+	return 0;
+}
