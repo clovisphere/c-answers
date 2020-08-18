c78109fd35be6dbd3bc8062785c66a97f9062de8
Clovis Mugaruka <clovis.mugaruka@gmail.com>
2020-08-18 13:08:56 +0300

    add: solution to exercise 1-12

---
+++ chapter_01/ex1_12.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+#define IN  1 // inside a word
+#define OUT 0 // outside a word
+
+/* print input one word per line */
+int main() {
+	int c;
+	while((c = getchar()) != EOF) {
+		// a word is sequence of characters that does not contaion
+		// a blank, tab or newline. 
+		if (c == ' ' || c == '\t' || c == '\n') {
+			printf("\n");
+			continue;
+		}
+		putchar(c);
+	}
+	return 0;
+}
