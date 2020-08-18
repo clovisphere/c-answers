0bcf9ce1083d37a4375987895bc0a5e4cde684cb
Clovis Mugaruka <clovis.mugaruka@gmail.com>
2020-08-18 17:43:56 +0300

    add: solution to exercise 1-14

---
+++ chapter_01/ex1_14.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+#define SIZE 26 // maximum size of the array
+
+/* histogram of the frequency of different characters in input */
+int main() {
+	int c, count = 0, map[SIZE] = {0};
+	char ch[SIZE] = {'\0'};
+	bool seen = false;
+
+	while ((c = getchar()) != EOF) {
+		if (c == ' ' || c == '\t' || c == '\n') continue; /* skip: ignore blank */
+		else {
+			for (int i = 0; i < SIZE; i++) {
+				if (ch[i] == c) {
+					seen = true;
+					++map[i];
+					break;
+				}
+				seen = false;
+			}
+			if (!seen) {
+				ch[count] = c;
+				++map[count];
+				++count;
+			}
+		}
+	}
+	printf("\n");
+    // histogram - horizontal orientation
+	for (int i = 0; i < SIZE; i++) {
+		if (map[i] == 0) continue; /* skip */
+		printf("%c: ", ch[i]);
+		for(int j = 0; j < map[i]; j++)
+			printf("*");
+		printf("\n");
+	}
+	return 0;
+}
